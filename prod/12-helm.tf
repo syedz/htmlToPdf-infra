77a60363a966749bb3ae77b7333f303c088c13fb
Zeeshan Syed <zsyed89@gmail.com>
2025-12-24 13:59:34 -0500

    Fixed set syntax

--- prod/12-helm.tf
+++ prod/12-helm.tf
@@ -12,17 +12,18 @@ resource "helm_release" "ack-lambda" {
   create_namespace = "true"
   timeout          = 600
 
-  set {
-    name  = "aws.region"
-    value = var.aws_region
-    type  = "string"
-  }
-
-  set {
-    name  = "defaultResyncPeriod"
-    value = "0"
-    type  = "string"
-  }
+  set = [
+    {
+      name  = "aws.region"
+      value = var.aws_region
+      type  = "string"
+    },
+    {
+      name  = "defaultResyncPeriod"
+      value = "0"
+      type  = "string"
+    }
+  ]
 
   depends_on = [module.eks, data.aws_eks_cluster.cluster]
 
@@ -36,26 +37,29 @@ resource "helm_release" "crd-helm-chart" {
   chart            = "./crd-helm-chart"
   namespace        = "crd-helm-chart"
   create_namespace = "true"
-  set {
-    name  = "tag_env"
-    value = var.tag_env
-    type  = "string"
-  }
-  set {
-    name  = "projectrepo"
-    value = var.ci_project_repo
-    type  = "string"
-  }
-  set {
-    name  = "aws_account_id"
-    value = data.aws_caller_identity.current.account_id
-    type  = "string"
-  }
-  set {
-    name  = "aws_region"
-    value = var.aws_region
-    type  = "string"
-  }
+  
+  set = [
+    {
+      name  = "tag_env"
+      value = var.tag_env
+      type  = "string"
+    },
+    {
+      name  = "projectrepo"
+      value = var.ci_project_repo
+      type  = "string"
+    },
+    {
+      name  = "aws_account_id"
+      value = data.aws_caller_identity.current.account_id
+      type  = "string"
+    },
+    {
+      name  = "aws_region"
+      value = var.aws_region
+      type  = "string"
+    }
+  ]
   
   depends_on = [helm_release.actions-runner-controller, helm_release.ack-lambda, module.eks]
 }
@@ -68,11 +72,14 @@ resource "helm_release" "cert-manager" {
   namespace        = "cert-manager"
   version          = "v1.19.2"
   create_namespace = "true"
-  set {
-    name  = "installCRDs"
-    value = "true"
-    type  = "string"
-  }
+  
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+      type  = "string"
+    }
+  ]
 
   depends_on = [module.eks, data.aws_eks_cluster.cluster]
 
@@ -90,16 +97,18 @@ resource "helm_release" "actions-runner-controller" {
   version          = "0.23.7"
   create_namespace = "true"
 
-  set {
-    name  = "authSecret.create"
-    value = "true"
-    type  = "string"
-  }
-  set {
-    name  = "authSecret.github_token"
-    value = var.registrationToken
-    type  = "string"
-  }
+  set = [
+    {
+      name  = "authSecret.create"
+      value = "true"
+      type  = "string"
+    },
+    {
+      name  = "authSecret.github_token"
+      value = var.registrationToken
+      type  = "string"
+    }
+  ]
 
   depends_on = [helm_release.cert-manager, module.eks]
 }
@@ -113,50 +122,62 @@ resource "helm_release" "ingress-nginx" {
   namespace  = "ingress-nginx"
   create_namespace = "true"
   timeout    = 600
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-connection-idle-timeout"
-    value = "60"
-    type  = "string"
-  }
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
-    value = "true"
-    type  = "string"
-  }
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"
-    value = aws_acm_certificate.eks_domain_cert.arn
-    type  = "string"
-  }
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"
-    value = "https"
-    type  = "string"
-  }
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-negotiation-policy"
-    value = "ELBSecurityPolicy-TLS-1-2-2017-01"
-    type  = "string"
-  }
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-    value = "nlb"
-    type  = "string"
-  }
-  set {
-    name  = "controller.service.targetPorts.http"
-    value = "http"
-    type  = "string"
-  }
-  set {
-    name  = "controller.service.targetPorts.https"
-    value = "http"
-    type  = "string"
-  }
-  set {
-    name  = "controller.admissionWebhooks.enabled"
-    value = "false"
-  }
+
+  set = [
+    {
+
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-connection-idle-timeout"
+      value = "60"
+      type  = "string"
+    },
+    {
+
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
+      value = "true"
+      type  = "string"
+    },
+    {
+
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"
+      value = aws_acm_certificate.eks_domain_cert.arn
+      type  = "string"
+    },
+    {
+
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"
+      value = "https"
+      type  = "string"
+    },
+    {
+
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-negotiation-policy"
+      value = "ELBSecurityPolicy-TLS-1-2-2017-01"
+      type  = "string"
+    },
+    {
+
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+      value = "nlb"
+      type  = "string"
+    },
+    {
+
+      name  = "controller.service.targetPorts.http"
+      value = "http"
+      type  = "string"
+    },
+    {
+
+      name  = "controller.service.targetPorts.https"
+      value = "http"
+      type  = "string"
+    },
+    {
+
+      name  = "controller.admissionWebhooks.enabled"
+      value = "false"
+    }
+  ]
 
   depends_on = [helm_release.cert-manager, module.eks, aws_acm_certificate_validation.eks_domain_cert_validation, data.aws_eks_cluster.cluster]
 
@@ -175,37 +196,37 @@ resource "helm_release" "argocd" {
   repository       = "https://argoproj.github.io/argo-helm"
   timeout          = 300
 
-  set {
-    name  = "server.extraArgs"
-    value = "{--insecure}"
-  }
-
-  set {
-    name  = "server.ingress.enabled"
-    value = "true"
-    type  = "string"
-  }
-
-  set {
-    name  = "server.ingress.annotations.kubernetes\\.io/ingress\\.class"
-    value = "nginx"
-    type  = "string"
-  }
-  set {
-    name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/force-ssl-redirect"
-    value = "false"
-    type  = "string"
-  }
-  set {
-    name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/backend-protocol"
-    value = "HTTP"
-    type  = "string"
-  }
-  set {
-    name  = "server.ingress.hosts"
-    value = "{argo.${var.tag_env}.${var.base_domain}}"
-    type  = "string"
-  }
+  set = [
+    {
+      name  = "server.extraArgs"
+      value = "{--insecure}"
+    },
+    {
+      name  = "server.ingress.enabled"
+      value = "true"
+      type  = "string"
+    },
+    {
+      name  = "server.ingress.annotations.kubernetes\\.io/ingress\\.class"
+      value = "nginx"
+      type  = "string"
+    },
+    {
+      name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/force-ssl-redirect"
+      value = "false"
+      type  = "string"
+    },
+    {
+      name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/backend-protocol"
+      value = "HTTP"
+      type  = "string"
+    },
+    {
+      name  = "server.ingress.hosts"
+      value = "{argo.${var.tag_env}.${var.base_domain}}"
+      type  = "string"
+    }
+  ]
 
   depends_on = [module.eks, data.aws_eks_cluster.cluster]
 
@@ -223,16 +244,19 @@ resource "helm_release" "argocd-apps" {
   repository = "https://argoproj.github.io/argo-helm"
   timeout    = 300
 
-  set {
-    name  = "applications[0].source.repoURL"
-    value = "https://github.com/${var.cd_project_repo}"
-    type  = "string"
-  }
-  set {
-    name  = "applications[0].source.targetRevision"
-    value = var.tag_env
-    type  = "string"
-  }
+  set = [
+    {
+      name  = "applications[0].source.repoURL"
+      value = "https://github.com/${var.cd_project_repo}"
+      type  = "string"
+    },
+    {
+      name  = "applications[0].source.targetRevision"
+      value = var.tag_env
+      type  = "string"
+    }
+  ]
+  
   values = [
     "${file("helm-chart-values/argo-cd-apps-values.yaml")}"
   ]
@@ -253,34 +277,37 @@ resource "helm_release" "datadog" {
   version          = "3.157.1"
   create_namespace = "true"
   timeout          = 300
-  set {
-    name  = "datadog.apiKey"
-    value = var.datadog_api_key
-  }
-  set {
-    name  = "datadog.appkey"
-    value = var.datadog_application_key
-  }
-  set {
-    name  = "datadog.clusterName"
-    value = lower(module.eks.cluster_name)
-  }
-  set {
-    name  = "datadog.site"
-    value = var.datadog_region
-  }
-  set {
-    name  = "datadog.dd_url"
-    value = "https://${var.datadog_region}"
-  }
-  set {
-    name  = "datadog.logs.enabled"
-    value = "true"
-  }
-  set {
-    name  = "datadog.logs.containerCollectAll"
-    value = "true"
-  }
+  
+  set = [
+    {
+      name  = "datadog.apiKey"
+      value = var.datadog_api_key
+    },
+    {
+      name  = "datadog.appkey"
+      value = var.datadog_application_key
+    },
+    {
+      name  = "datadog.clusterName"
+      value = lower(module.eks.cluster_name)
+    },
+    {
+      name  = "datadog.site"
+      value = var.datadog_region
+    },
+    {
+      name  = "datadog.dd_url"
+      value = "https://${var.datadog_region}"
+    },
+    {
+      name  = "datadog.logs.enabled"
+      value = "true"
+    },
+    {
+      name  = "datadog.logs.containerCollectAll"
+      value = "true"
+    }
+  ]
 
   depends_on = [module.eks, data.aws_eks_cluster.cluster]
 
